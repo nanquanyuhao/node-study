b3d5c0aa7db2530f2ccfb8dd6131cffd28089da0
nanquanyuhao <nanquanyuhao@foxmail.com>
2022-03-24 19:28:52 +0800

    基础路由模块加入测试

--- web/koa/url/start.sh
+++
@@ -1,2 +0,0 @@
-# node app.js 也可，由于使用 package.json 使用包管理，可以用以下命令
-npm start
---
+++ web/koa/url/url-koa/app.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const Koa = require('koa');
+
+// 注意require('koa-router')返回的是函数:
+// 相当于如下：
+// const fn_router = require('koa-router');
+// const router = fn_router();
+const router = require('koa-router')();
+
+const app = new Koa();
+
+const bodyParser = require('koa-bodyparser');
+// 使用 post 请求体解析功能
+app.use(bodyParser());
+
+// log request URL:
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+});
+
+// add url-route:
+router.get('/hello/:name', async (ctx, next) => {
+    var name = ctx.params.name;
+    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get('/', async (ctx, next) => {
+    ctx.response.body = `<h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"></p>
+            <p>Password: <input name="password" type="password"></p>
+            <p><input type="submit" value="Submit"></p>
+        </form>`;
+});
+
+router.post('/signin', async (ctx, next) => {
+    var
+        name = ctx.request.body.name || '',
+        password = ctx.request.body.password || '';
+    console.log(`signin with name: ${name}, password: ${password}`);
+    if (name === 'koa' && password === '12345') {
+        ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
+    } else {
+        ctx.response.body = `<h1>Login failed!</h1>
+        <p><a href="/">Try again</a></p>`;
+    }
+});
+
+// add router middleware:
+app.use(router.routes());
+
+app.listen(3000);
+console.log('app started at port 3000...');
