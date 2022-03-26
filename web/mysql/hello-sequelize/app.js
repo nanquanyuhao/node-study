0bbb48eb04c60b4dcb284e99893075cdc88d1851
nanquanyuhao <nanquanyuhao@foxmail.com>
2022-03-27 00:47:07 +0800

    基础mysql使用完成学习

---
+++ web/mysql/hello-sequelize/app.js
@@ -0,0 +1,87 @@
+const Sequelize = require('sequelize');
+const config = require('./config');
+
+var sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 30000
+    }
+});
+
+// 赋值模型数据 Model
+var Pet = sequelize.define('pet', {
+    id: {
+        type: Sequelize.STRING(50),
+        primaryKey: true
+    },
+    name: Sequelize.STRING(100),
+    gender: Sequelize.BOOLEAN,
+    birth: Sequelize.STRING(10),
+    createdAt: Sequelize.BIGINT,
+    updatedAt: Sequelize.BIGINT,
+    version: Sequelize.BIGINT
+}, {
+        // 关闭 Sequelize 的自动添加 timestamp 的功能
+        timestamps: false
+    });
+
+var now = Date.now();
+
+// 往数据库插入数据
+Pet.create({
+    id: 'g-' + now,
+    name: 'Gaffey',
+    gender: false,
+    birth: '2007-07-07',
+    createdAt: now,
+    updatedAt: now,
+    version: 0
+}).then(function (p) {
+    console.log('created.' + JSON.stringify(p));
+}).catch(function (err) {
+    console.log('failed: ' + err);
+});
+
+// await 方式插入数据
+(async () => {
+    var dog = await Pet.create({
+        id: 'd-' + now,
+        name: 'Odie',
+        gender: false,
+        birth: '2008-08-08',
+        createdAt: now,
+        updatedAt: now,
+        version: 0
+    });
+    console.log('created: ' + JSON.stringify(dog));
+})();
+
+// 查询数据
+(async () => {
+
+    // 返回的一个或一组对象称： Model 实例
+    var pets = await Pet.findAll({
+        where: {
+            name: 'Gaffey'
+        }
+    });
+    console.log(`find ${pets.length} pets:`);
+    for (let p of pets) {
+        console.log(JSON.stringify(p));
+
+        console.log('update pet...');
+        p.gender = true;
+        p.updatedAt = Date.now();
+        p.version ++;
+        await p.save();
+
+        // 删除测试
+        if (p.version === 3) {
+            await p.destroy();
+            console.log(`${p.name} was destroyed.`);
+        }
+    }
+})();
---
+++ web/mysql/hello-sequelize/config.js
@@ -0,0 +1,9 @@
+var config = {
+    database: 'test',
+    username: 'liyh1928',
+    password: '123456',
+    host: '192.168.235.136',
+    port: 3306
+};
+
+module.exports = config;
