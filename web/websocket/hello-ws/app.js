524d351f74034833e98c7f7ee74d6f0e269ad74b
nanquanyuhao <nanquanyuhao@foxmail.com>
2022-03-27 14:44:44 +0800

    WebSocket 示例以及聊天室项目完成

---
+++ web/websocket/hello-ws/app.js
@@ -0,0 +1,41 @@
+// 导入WebSocket模块:
+const WebSocket = require('ws');
+
+// 引用Server类:
+const WebSocketServer = WebSocket.Server;
+
+// 实例化:
+const wss = new WebSocketServer({
+    port: 3000
+});
+
+wss.on('connection', function (ws) {
+
+    console.log(`[SERVER] connection()`);
+    ws.on('message', function (message) {
+
+        console.log(`[SERVER] Received: ${message}`);
+        ws.send(`ECHO: ${message}`, (err) => {
+            if (err) {
+                console.log(`[SERVER] error: ${err}`);
+            }
+        });
+    })
+});
+
+console.log('ws server started at port 3000...');
+
+// client test:
+// 打开一个WebSocket:
+let ws = new WebSocket('ws://localhost:3000/test');
+
+// 打开WebSocket连接后立刻发送一条消息:
+ws.on('open', function () {
+    console.log(`[CLIENT] open()`);
+    ws.send('Hello!');
+});
+
+// 响应收到的消息:
+ws.on('message', function (message) {
+    console.log(`[CLIENT] Received: ${message}`);
+});
